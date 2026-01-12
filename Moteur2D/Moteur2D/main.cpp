2259d7408b0c0a7288aa584cf711df8054d7057d
AleX-X07 <asoulier@gaming.tech>
2026-01-12 10:16:48 +0100

    Init SDL

--- Moteur2D/Moteur2D/main.cpp
+++ Moteur2D/Moteur2D/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <SDL3/SDL.h>
+
+bool keepGoing = true;
 
 int main() {
 
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer("Moteur2D", 1920, 1080, SDL_WINDOW_FULLSCREEN, &window, &renderer);
+	
+	while(keepGoing) {
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		SDL_RenderFillRect(renderer, NULL);
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
 }
