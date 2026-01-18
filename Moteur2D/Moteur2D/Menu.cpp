ee7a1ec99eeda37c29545c63ab03e5a14e2eff10
Alex <asoulier@gaming.tech>
2026-01-18 13:51:00 +0100

    Loading picture is good in menu

--- Moteur2D/Moteur2D/Menu.cpp
+++ Moteur2D/Moteur2D/Menu.cpp
@@ -8,17 +8,12 @@ Menu::~Menu() {
 }
 
 void Menu::displayScene(LoadRessources& _MyRessources) {
-	rect = { 0, 0, 1920, 1080 };
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	
 	SDL_RenderClear(renderer);
 
 	if (_MyRessources.bg_layer4) {  // Vérifiez que la texture existe
-		SDL_RenderTexture(renderer, _MyRessources.bg_layer4, nullptr, &rect);
-		printf("image charger");
+		SDL_RenderTexture(renderer, _MyRessources.bg_layer4, nullptr, nullptr);
 	}
-
-
-	SDL_RenderFillRect(renderer, nullptr);
 
 	SDL_RenderPresent(renderer);
 }
